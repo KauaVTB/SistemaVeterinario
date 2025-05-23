9d2c37b0c37cb39e058b8911cc9af77ce5461b75
KauãViniciusTB <kaua2810ar@gmail.com>
2025-05-23 18:21:16 -0300

    Initial commit

---
+++ DataBase.php
@@ -0,0 +1,22 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $port = '3306';
+    private $db_name = 'vet_clinic';
+    private $username = 'root';
+    private $password = '123456789';
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};port={$this->port};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+            //echo "Conexão realizada com sucesso!<br>";
+        } catch(PDOException $exception) {
+            echo "Erro de conexão: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
